c3c012cd686cc3cd39cdb65c57579addababb7e6
omharinand <shraddhanand_m@infosys.com>
2016-02-19 15:38:51 +0530

    Update Bowling\src\com\vf\bowling\model\FinalScore.java

--- Bowling\src\com\vf\bowling\model\FinalScore.java
+++ Bowling\src\com\vf\bowling\model\FinalScore.java
@@ -1,1 +1,12 @@
+package com.vf.bowling.model;
 
+public class FinalScore extends Score {
+
+    public FinalScore(int firstScore, int secondScore) {
+        super(firstScore, secondScore);
+    }
+
+    public FinalScore(int firstScore) {
+        super(firstScore);
+    }
+}
